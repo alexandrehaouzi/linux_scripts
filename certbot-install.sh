f5537f9a8c7703d086cdc9e769ddf7010dbac787
alexandrehaouzi <alexandre.haouzi@gmail.com>
2020-06-04 13:15:48 +0200

    add certbot auto

---
+++ certbot-install.sh
@@ -0,0 +1,25 @@
+#! bin/bash
+
+echo "UPDATE"
+
+sudo apt-get update
+
+echo "INSTALL DEPENDENCES"
+
+sudo apt-get install software-properties-common
+
+echo "ADD APT"
+
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+
+echo "UPDATE"
+
+sudo apt-get update
+
+echo "INSTALL CERTBOT"
+
+sudo apt-get install certbot python3-certbot-apache
+sudo certbot --apache
+
+echo "INSTALL CERTBOT OK"
